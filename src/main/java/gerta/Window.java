a9b8d5478a8a942d7bd24c999d86f69027428acf
Alex Vashchenko <asdw3a@mail.ru>
2021-08-24 17:26:36 +0300

    Deserialization with Gson

--- src/main/java/gerta/Window.java
+++ src/main/java/gerta/Window.java
@@ -159,6 +159,7 @@ public class Window {
         float endTime;
         float dt = -1.0f;
 
+        currentScene.load();
         while (!glfwWindowShouldClose(glfwWindow)){
             //Poll events
             glfwPollEvents();
@@ -181,6 +182,7 @@ public class Window {
             dt = endTime - beginTime;
             beginTime = endTime;
         }
+        currentScene.saveExit();
     }
 
     public static int getWidth(){
