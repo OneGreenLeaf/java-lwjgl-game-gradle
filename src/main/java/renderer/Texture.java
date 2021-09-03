71a891ab12663e10fac50b9288e10a905626eea0
Alex Vashchenko <asdw3a@mail.ru>
2021-09-03 13:14:29 +0300

    fix class Texture

--- src/main/java/renderer/Texture.java
+++ src/main/java/renderer/Texture.java
@@ -29,14 +29,45 @@ public class Texture {
 
 
     public  void init(String filepath){
-        this.filepath = "Generated";
+        this.filepath = filepath;
 
         //Generate texture on GPU
         texID = glGenTextures();
         glBindTexture(GL_TEXTURE_2D, texID);
 
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height,
-                0, GL_RGB, GL_UNSIGNED_BYTE, 0);
+        //Set texture parameters
+        //Repeat image in both directions
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        //When stretching the image, pixelate
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        //When shrinking an image, pixelate
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+        IntBuffer width = BufferUtils.createIntBuffer(1);
+        IntBuffer height = BufferUtils.createIntBuffer(1);
+        IntBuffer channels = BufferUtils.createIntBuffer(1);
+        stbi_set_flip_vertically_on_load(true);
+        ByteBuffer image = stbi_load(filepath, width, height, channels, 0);
+
+        if (image != null){
+            this.width = width.get(0);
+            this.height = height.get(0);
+
+            if (channels.get(0) == 3){
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width.get(0), height.get(0),
+                        0, GL_RGB, GL_UNSIGNED_BYTE, image);
+            } else if (channels.get(0) == 4) {
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width.get(0),height.get(0),
+                        0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+            } else {
+                assert false : "Error: (Texture) Unknown number of channels '" + channels.get(0) + "'";
+            }
+        } else {
+            assert false : "Error: (Texture) Could not load image '" + filepath + "'";
+        }
+
+        stbi_image_free(image);
     }
 
 
